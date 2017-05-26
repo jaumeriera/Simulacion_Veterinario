9dd23340aa8fee3245ba9452c262cfea58662e5b
Jaumerg95 <jaumerg95@gmail.com>
2017-05-26 13:50:23 +0200

    Ads de la cola de prioridad y implementacion de is_empty, empty y get_least

--- dheap.adb
+++ dheap.adb
@@ -5,15 +5,15 @@ package body dheap is
     q.n := 0;
   end empty;
 
-  function is_empty (q : out heap) is
+  function is_empty (q : out heap) return boolean is
   begin
     return q.n = 0;
   end is_empty;
 
-  function get_last (q : in heap) is
+  function get_last (q : in heap) return item is
   begin
     if is_empty(q) then raise bad_use; end if;
-    return q.memory(1);
+    return q.memory(1).x;
   end get_last;
 
 
